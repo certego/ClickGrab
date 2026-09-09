48efbef579ceed347a56083bf982d54754978530
InOr3p <manu.conforti@gmail.com>
2026-09-09 14:54:20 +0200

    Fix

--- __init__.py
+++ __init__.py
@@ -1,7 +1,10 @@
-# __init__.py
-import sys
 import os
+import sys
 
 _pkg_dir = os.path.dirname(os.path.abspath(__file__))
 if _pkg_dir not in sys.path:
     sys.path.insert(0, _pkg_dir)
+
+from .clickgrab import analyze_url, main
+
+__all__ = ["analyze_url", "main"]
